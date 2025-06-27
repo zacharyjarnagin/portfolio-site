ab2b3a553acdd2b3edee42a2ba0075341374f078
Zachary Jarnagin <zjarnagin1@gmail.com>
2025-06-26 20:44:44 -0400

    follow MUI performance suggestions

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,12 @@
-'use client';
-import { createTheme } from '@mui/material/styles';
-import type {} from '@mui/lab/themeAugmentation';
+"use client";
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   colorSchemes: {
     dark: true,
   },
   typography: {
-    fontFamily: 'var(--font-roboto)',
+    fontFamily: "var(--font-roboto)",
   },
   cssVariables: true,
 });
